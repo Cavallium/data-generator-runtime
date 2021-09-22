ff703453f88f3b11b7f069b785767414199dbd28
Andrea Cavalli <andrea@cavallium.it>
2021-09-22 18:26:29 +0200

    Annotate Nullable

--- src/main/java/it/cavallium/data/generator/DataSerializer.java
+++ src/main/java/it/cavallium/data/generator/DataSerializer.java
@@ -4,10 +4,11 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface DataSerializer<T> {
 
-	void serialize(DataOutput dataOutput, @NotNull T data) throws IOException;
+	void serialize(DataOutput dataOutput, @Nullable T data) throws IOException;
 
 	@NotNull T deserialize(DataInput dataInput) throws IOException;
 }
